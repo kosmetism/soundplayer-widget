64d0547c4cf2040282ba96ff51b8e462d20e965d
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2015-05-07 15:01:36 +0300

    start with components, updates

---
+++ src/components/PlayButton.js
@@ -0,0 +1,45 @@
+/** @jsx deku.dom */
+
+import deku from 'deku';
+
+const PlayButton = {
+    defaultProps: {
+        playing: false,
+        seeking: false
+    },
+
+    propTypes: {
+        playing: {
+            type: 'boolean'
+        },
+        seeking: {
+            type: 'boolean'
+        }
+    },
+
+    render(component) {
+        const { props } = component;
+
+        function handleClick (e) {
+            e.preventDefault();
+
+            const { playing, soundCloudAudio, onTogglePlay } = props;
+
+            if (!playing) {
+                soundCloudAudio && soundCloudAudio.play();
+            } else {
+                soundCloudAudio && soundCloudAudio.pause();
+            }
+
+            onTogglePlay && onTogglePlay(e);
+        }
+
+        return (
+            <button class="soundplayer-play-button" onClick={handleClick}>
+                {props.playing ? 'Pause' : 'Play'}
+            </button>
+        );
+    }
+};
+
+export default PlayButton;
--- src/widget.js
+++ src/widget.js
@@ -2,13 +2,36 @@
 
 import deku from 'deku';
 
-export function create (el, opts) {
-    let app = deku.scene();
+import PlayButton from './components/PlayButton';
 
-    app.mount(
-        <div>
-            Widget Content!
-        </div>
+const Widget = {
+    initialState() {
+        return {
+            playing: false
+        };
+    },
+
+    render(component, setState) {
+        let { props, state } = component;
+
+        console.log(props);
+
+        function handleClick () {
+            setState({playing: !!!state.playing});
+        }
+
+        return (
+            <div>
+                Widget Content!
+                <PlayButton playing={state.playing} onTogglePlay={handleClick} />
+            </div>
+        );
+    }
+};
+
+export function create (el, opts) {
+    let app = deku.scene(
+        <Widget url={opts.url} />
     );
 
     deku.render(app, el);
